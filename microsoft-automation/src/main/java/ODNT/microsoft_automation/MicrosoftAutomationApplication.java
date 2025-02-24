573c52694dff5e098710ca1a8932787eeb7a6e8b
Adam Pena <adamsorganizer@gmail.com>
2025-02-24 13:22:22 -0600

    25 minutes of work. Maven back bone and pom.xml file updated to reflect dependencies for Microsoft365, and MySQL (with backup SQLite commented out)

---
+++ microsoft-automation/src/main/java/ODNT/microsoft_automation/MicrosoftAutomationApplication.java
@@ -0,0 +1,13 @@
+package ODNT.microsoft_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosoftAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosoftAutomationApplication.class, args);
+	}
+
+}
---
+++ microsoft-automation/src/test/java/ODNT/microsoft_automation/MicrosoftAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package ODNT.microsoft_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosoftAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
